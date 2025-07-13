1956ca0ca183e5496e06ce34b196e21a70a73d20
robetchens <163840379+robetchens@users.noreply.github.com>
2025-07-13 16:22:05 +0700

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Kinderkloud-Little Maestro - Virtual Studio-0.1";
+const contentToCache = [
+    "Build/Build Web Gl.loader.js",
+    "Build/Build Web Gl.framework.js.unityweb",
+    "Build/Build Web Gl.data.unityweb",
+    "Build/Build Web Gl.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
